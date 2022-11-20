99ddc242cb9999d28fe6c8178e8b9571e511f3e4
Jordi Pakey-Rodriguez <hi@jordi.id>
2022-11-20 00:01:51 -0800

    Add support for MIDI mute button

--- keyboards/pikatea/pikatea_macropad_fk1/keymaps/midi/keymap.c
+++ keyboards/pikatea/pikatea_macropad_fk1/keymaps/midi/keymap.c
@@ -1,4 +1,5 @@
-/* Copyright 2022 Jack Kester
+/* Copyright 2020 Jack Kester
+ * Copyright 2022 Jordi Pakey-Rodriguez
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,48 +17,70 @@
 
 #include QMK_KEYBOARD_H
 
+#define MIDI_CC_VOLUME 7
+#define MIDI_CC_MUTE 120
+
 /*
-This keymap mulates a MIDI slider with the encoder. Changing layers
+This keymap emulates a MIDI slider with the encoder. Changing layers
 changes the Midi channel used. Compatible with Vial.
 */
 extern MidiDevice midi_device;
 // Channel values
-uint8_t channelVal[6] = {0, 0, 0, 0, 0, 0};
+uint8_t channelVal[6] = {127, 127, 127, 127, 127, 127};
 // Step size
 uint8_t stepSize = 2;
 // Current layer
 uint8_t currentLayer = 0;
 
+enum keycodes {
+    ENC_BTN = SAFE_RANGE
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        LT(1, KC_F15),    LT(2, KC_F16),    LT(3, KC_MPRV),    LT(4, KC_MPLY),    LT(5, KC_MNXT),    KC_MUTE
+        LT(1,KC_F15), LT(2,KC_F16), LT(3,KC_MPRV), LT(4,KC_MPLY), LT(5,KC_MNXT), ENC_BTN
     ),
 };
+
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case ENC_BTN:
+        if (record->event.pressed) {
+            if (currentLayer == 0) {
+                tap_code(KC_MUTE);
+            }
+            midi_send_cc(&midi_device, currentLayer, MIDI_CC_MUTE, 0);
+        }
+        return false; // Skip all further processing of this key
+    default:
+        return true; // Process all other keycodes normally
+    }
+}
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (currentLayer == 0) {
         tap_code(clockwise ? KC_VOLU : KC_VOLD);
-    } else {
-        if (clockwise) {
-            channelVal[currentLayer] += stepSize;
-            if (channelVal[currentLayer] > 127) {
-                channelVal[currentLayer] = 127;
-            }
-            midi_send_cc(&midi_device, currentLayer + 1, 7, channelVal[currentLayer]);
-        } else {
-            if (channelVal[currentLayer] >= stepSize) {
-                channelVal[currentLayer] -= stepSize;
-            } else {
-                channelVal[currentLayer] = 0;
-            }
-            midi_send_cc(&midi_device, currentLayer + 1, 7, channelVal[currentLayer]);
+    }
+    if (clockwise) {
+        channelVal[currentLayer] += stepSize;
+        if (channelVal[currentLayer] > 127) {
+            channelVal[currentLayer] = 127;
         }
+        midi_send_cc(&midi_device, currentLayer, MIDI_CC_VOLUME, channelVal[currentLayer]);
+    } else {
+        channelVal[currentLayer] -= stepSize;
+        if (channelVal[currentLayer] > 127) {
+            channelVal[currentLayer] = 0;
+        }
+        midi_send_cc(&midi_device, currentLayer, MIDI_CC_VOLUME, channelVal[currentLayer]);
     }
     return true;
 }
 
-//save the layer state to a variable
 layer_state_t layer_state_set_user(layer_state_t state) {
     currentLayer = get_highest_layer(state);
     return state;
--- keyboards/pikatea/pikatea_macropad_fk1/keymaps/midi/rules.mk
+++ keyboards/pikatea/pikatea_macropad_fk1/keymaps/midi/rules.mk
@@ -1,6 +1,7 @@
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
 VIAL_INSECURE = yes
+ENCODER_MAP_ENABLE = yes
 # Reduce firmware size. Change these depending upon what you need.
 QMK_SETTINGS = yes
 TAP_DANCE_ENABLE = yes
